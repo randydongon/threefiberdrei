0bccdf9dc7caf44dfd30a2b0002e849743fb73a0
randydongon <bodrorandy@gmail.com>
2022-02-02 20:40:17 +0800

    Updates

---
+++ precache-manifest.d3e55164901803aec1b147ae896ced15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "854f43f71d3de0f80199e1e6f5ba7712",
+    "url": "/threefiberdrei/index.html"
+  },
+  {
+    "revision": "3de5bd2798591bd91dd3",
+    "url": "/threefiberdrei/static/css/main.c0794a18.chunk.css"
+  },
+  {
+    "revision": "8f7203e360738c5d7142",
+    "url": "/threefiberdrei/static/js/2.99d63aeb.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/threefiberdrei/static/js/2.99d63aeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3de5bd2798591bd91dd3",
+    "url": "/threefiberdrei/static/js/main.bc768606.chunk.js"
+  },
+  {
+    "revision": "ad2686c944aa28424eac",
+    "url": "/threefiberdrei/static/js/runtime-main.e358a063.js"
+  }
+]);
